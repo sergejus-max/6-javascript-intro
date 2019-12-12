e65ee23de4effe87c866826515703e22265df7a5
pacifist <lavrovij@list.ru>
2019-12-12 13:47:28 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+console.log(1,2,3,4);
+
+//tekstai
+console.log('a', 'b', 'c', 'd');
+
+//boolean - logine reiksme
+console.log(true);
+console.log(false);
+
+//sarasai - arrey
+console.log([1,2,3,4,5]);
+console.log(['a','b','c','d']);
+console.log(['a',98,'b',253,[1,2,'asd'],'c',546,'d'])
+
+//objektai - object
+console.log({name: 'Sergej',age: 38});
+
+
+
+
+
