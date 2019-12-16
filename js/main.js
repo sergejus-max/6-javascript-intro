ca74918ef0a8d4fd6287be34d1f9db9f586eb056
pacifist <lavrovij@list.ru>
2019-12-16 14:55:52 +0200

    ciklai

--- js/main.js
+++ js/main.js
@@ -1,61 +1,103 @@
-//  kaip isnesti pranesima i konsole
-console.log('labas rytas');
-console.log('ka tu?');
-console.log('nieko, o tu?');
+// //  kaip isnesti pranesima i konsole
+// console.log('labas rytas');
+// console.log('ka tu?');
+// console.log('nieko, o tu?');
 
-//sudedamosios dalys:
-// -kintamieji
-// -funkcijos 
-// - event'ai (listener'iai)
+// //sudedamosios dalys:
+// // -kintamieji
+// // -funkcijos 
+// // - event'ai (listener'iai)
 
-const bruk = '---------------------';
-console.log(bruk);
+// const bruk = '---------------------';
+// console.log(bruk);
 
-// Kintamieji negali prasideti skaiciumi, arba zenklu (+-*/?!). Gali buti (_, $ )
-// Kintamieji rasomi is mazosios raidies;
-//Kintamieji su teksto tipo reiksmemis;
-//kintamieji su saraso tipo reiksmemis, kuriuose yra is skaiciaus tipo, ir teksto tipo reiksmiu;
-// Skaiciaus tipas
- const PI = 3.1415;
- console.log( PI );
+// // Kintamieji negali prasideti skaiciumi, arba zenklu (+-*/?!). Gali buti (_, $ )
+// // Kintamieji rasomi is mazosios raidies;
+// //Kintamieji su teksto tipo reiksmemis;
+// //kintamieji su saraso tipo reiksmemis, kuriuose yra is skaiciaus tipo, ir teksto tipo reiksmiu;
+// // Skaiciaus tipas
+  const PI = 3.1415;
+  console.log( PI );
 
- let amzius = 0;    //LEt kintamuoju gales naudotis visi, kas yra tam paciam kodo gylije arba gylaiu esantis. 
- console.log('mano amzius yra', amzius);
- amzius = 7;
- console.log('mano amzius yra', amzius);
+//  let amzius = 0;    //LEt kintamuoju gales naudotis visi, kas yra tam paciam kodo gylije arba gylaiu esantis. 
+//  console.log('mano amzius yra', amzius);
+//  amzius = 7;
+//  console.log('mano amzius yra', amzius);
 
- // Teksto tipas
- var vardas = 'Sergej';     // VAR kintamuoju gales naudotis visi
- console.log('cia mano vardas:', vardas);
- vardas = 'Sergejus';
- console.log('nuo dabar mano vardas:', vardas);
+//  // Teksto tipas
+//  var vardas = 'Sergej';     // VAR kintamuoju gales naudotis visi
+//  console.log('cia mano vardas:', vardas);
+//  vardas = 'Sergejus';
+//  console.log('nuo dabar mano vardas:', vardas);
 
- // ARRAY - saraso tipas, masyvas
-const pirminiaiSkaiciai = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29 ];
-console.log(pirminiaiSkaiciai);
+//  // ARRAY - saraso tipas, masyvas
+// const pirminiaiSkaiciai = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29 ];
+// console.log(pirminiaiSkaiciai);
 
-const a = 8;
-const b = 'adsf';
-const c = [5248, 85463];
-const d = ['adjs', 'qwe', 'asdfg'];
-const e = [54236, 'awqhd', 45, ' lojku'];
-const f = [2, 'a', [1, 'dfr'], [a, b,] ];
-console.log(f);
+// const a = 8;
+// const b = 'adsf';
+// const c = [5248, 85463];
+// const d = ['adjs', 'qwe', 'asdfg'];
+// const e = [54236, 'awqhd', 45, ' lojku'];
+// const f = [2, 'a', [1, 'dfr'], [a, b,] ];
+// console.log(f);
 
 
-//VEIKSMAI SU KINTAMAISIAIS;
+// //VEIKSMAI SU KINTAMAISIAIS;
+
+// // const s1 = 15;
+// // const s2 = 33;
+// // const s3 = -18;
+// // const sum = s1 + s2 +s3;
+// // console.log(sum);
 
 // const s1 = 15;
 // const s2 = 33;
 // const s3 = -18;
-// const sum = s1 + s2 +s3;
+// const sum = s1 + s2 + 'a' + s3;
 // console.log(sum);
 
-const s1 = 15;
-const s2 = 33;
-const s3 = -18;
-const sum = s1 + s2 + 'a' + s3;
-console.log(sum);
+// //NAMU DARBAI;
+// // Veiksmai su kintamaisiais;
+
+// // 2 uzduotis;
+// const a1 = 'qwer';
+// const b1 = ' lpoik';
+// const c1 = ' defr';
+// const d1 = ' skirj'
+// let suma = a1 + b1 + c1 + d1;
+// console.log(suma);
+
+// //3 uzduotis;
+// const a2 = 1;
+// const b2 = -2;
+// const c2 = 3;
+// const d2 = -4;
+// const e2 = 5;
+// const suma2 = a2 + b2 + c2 + d2 + e2;
+// console.log(suma2);
+
+// // 
+// const nuo = 0;
+// const iki = 11;
+// const daliklis = 3;
+// let kiekis = 0;
+// fofor( let i = nuo; i<=iki ; i++ ) {
+//     console.log(i, i/daliklis);
+//     if (i % daliklis === 0) {
+
+//     }
+// }
+
+// //einam
+
+
+// console.log('skaiciu intervale tarp' + nuo + 'ir' + iki ',besidalijanciu be liekanos is' + daliklis + 'yra' + kiekis + 'vienetai.');
+
+for ( let i=0; i<5; i++) {
+    console.log(i);
+}
+
 
 
 
